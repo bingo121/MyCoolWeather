5ba91f30038c9037519a98d616f46cdc36ecff86
bingo <2994143266@qq.com>
2017-07-24 16:09:22 +0800

    firstday

--- app/src/main/java/com/example/administrator/mycoolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/mycoolweather/activity/MainActivity.java
@@ -1,7 +1,11 @@
-package com.example.administrator.mycoolweather;
+package com.example.administrator.mycoolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.mycoolweather.R;
+
+import org.litepal.LitePal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LitePal.getDatabase();
     }
 }
